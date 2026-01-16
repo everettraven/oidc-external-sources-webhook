36422dc6671fee68c5b75310650ecd1bf23a8582
Bryce Palmer <bpalmer@redhat.com>
2026-01-16 16:03:01 -0500

    (feature): achieve configuration parity with KAS
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- pkg/authenticator/jwt.go
+++ pkg/authenticator/jwt.go
@@ -2,17 +2,20 @@ package authenticator
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
-	"unsafe"
 
 	"github.com/everettraven/oidc-external-sources-webhook/pkg/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver"
 	apiserverv1 "github.com/everettraven/oidc-external-sources-webhook/pkg/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver/v1"
+	"github.com/everettraven/oidc-external-sources-webhook/pkg/internal/thirdparty/kubernetes/apiserver/pkg/apis/apiserver/validation"
 	"github.com/everettraven/oidc-external-sources-webhook/pkg/internal/thirdparty/kubernetes/apiserver/plugin/pkg/authenticator/token/oidc"
 	"github.com/spf13/pflag"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apiserver/pkg/authentication/authenticator"
+	"k8s.io/apiserver/pkg/authentication/token/union"
+	"k8s.io/apiserver/pkg/server/dynamiccertificates"
+	"sigs.k8s.io/yaml"
+
+	authenticationcel "k8s.io/apiserver/pkg/authentication/cel"
 )
 
 func NewJWT() *JWT {
@@ -37,30 +40,25 @@ func (j *JWT) Run(ctx context.Context) error {
 		return fmt.Errorf("configuration file must be specified for jwt authentication")
 	}
 
-	// TODO: hot-reload of configuration. For now, just load once on startup.
-	configBytes, err := os.ReadFile(j.configFile)
+	authnConfig, err := AuthenticationConfigurationFromConfigurationFile(j.configFile)
 	if err != nil {
-		return fmt.Errorf("reading configuration file: %w", err)
+		return fmt.Errorf("loading configuration: %w", err)
 	}
 
-	// TODO: Use AuthenticationConfiguration type instead.
-	config := &apiserverv1.JWTAuthenticator{}
-	// TODO: unmarshal from YAML
-	err = json.Unmarshal(configBytes, config)
+	out := &apiserver.AuthenticationConfiguration{}
+
+	err = apiserverv1.Convert_v1_AuthenticationConfiguration_To_apiserver_AuthenticationConfiguration(authnConfig, out)
 	if err != nil {
-		return fmt.Errorf("unmarshalling configuration: %w", err)
+		return fmt.Errorf("converting external representation to internal representation: %w", err)
 	}
 
-	out := &apiserver.JWTAuthenticator{}
-
-	err = Convert_v1_JWTAuthenticator_To_apiserver_JWTAuthenticator(config, out)
-	if err != nil {
-		return fmt.Errorf("converting: %w", err)
+	compiler := authenticationcel.NewDefaultCompiler()
+	fieldErrs := validation.ValidateAuthenticationConfiguration(compiler, out, nil)
+	if err := fieldErrs.ToAggregate(); err != nil {
+		return fmt.Errorf("validating authentication configuration: %w", err)
 	}
 
-	tokenAuthenticator, err := oidc.New(ctx, oidc.Options{
-		JWTAuthenticator: *out,
-	})
+	tokenAuthenticator, err := TokenAuthenticatorForAuthenticationConfiguration(ctx, out)
 	if err != nil {
 		return fmt.Errorf("creating token authenticator: %w", err)
 	}
@@ -70,58 +68,41 @@ func (j *JWT) Run(ctx context.Context) error {
 	return nil
 }
 
-func Convert_v1_JWTAuthenticator_To_apiserver_JWTAuthenticator(in *apiserverv1.JWTAuthenticator, out *apiserver.JWTAuthenticator) error {
-	err := Convert_v1_Issuer_To_apiserver_Issuer(&in.Issuer, &out.Issuer)
+func AuthenticationConfigurationFromConfigurationFile(cfgPath string) (*apiserverv1.AuthenticationConfiguration, error) {
+	// TODO: hot-reload of configuration. For now, just load once on startup.
+	configBytes, err := os.ReadFile(cfgPath)
 	if err != nil {
-		return fmt.Errorf("converting issuer: %w", err)
+		return nil, fmt.Errorf("reading configuration file: %w", err)
 	}
 
-	err = Convert_v1_ClaimMappings_To_apiserver_ClaimMappings(&in.ClaimMappings, &out.ClaimMappings)
+	config := &apiserverv1.AuthenticationConfiguration{}
+	err = yaml.Unmarshal(configBytes, config)
 	if err != nil {
-		return fmt.Errorf("converting claim mappings: %w", err)
+		return nil, fmt.Errorf("unmarshalling configuration: %w", err)
 	}
 
-	out.ClaimValidationRules = *(*[]apiserver.ClaimValidationRule)(unsafe.Pointer(&in.ClaimValidationRules))
-	out.UserValidationRules = *(*[]apiserver.UserValidationRule)(unsafe.Pointer(&in.UserValidationRules))
-
-	return nil
+	return config, nil
 }
 
-func Convert_v1_ClaimMappings_To_apiserver_ClaimMappings(in *apiserverv1.ClaimMappings, out *apiserver.ClaimMappings) error {
-	if err := Convert_v1_PrefixedClaimOrExpression_To_apiserver_PrefixedClaimOrExpression(&in.Username, &out.Username); err != nil {
-		return err
-	}
-	if err := Convert_v1_PrefixedClaimOrExpression_To_apiserver_PrefixedClaimOrExpression(&in.Groups, &out.Groups); err != nil {
-		return err
-	}
-	if err := Convert_v1_ClaimOrExpression_To_apiserver_ClaimOrExpression(&in.UID, &out.UID); err != nil {
-		return err
-	}
-	out.Extra = *(*[]apiserver.ExtraMapping)(unsafe.Pointer(&in.Extra))
-	return nil
-}
+func TokenAuthenticatorForAuthenticationConfiguration(ctx context.Context, cfg *apiserver.AuthenticationConfiguration) (authenticator.Token, error) {
+	jwtAuthenticators := []authenticator.Token{}
 
-func Convert_v1_PrefixedClaimOrExpression_To_apiserver_PrefixedClaimOrExpression(in *apiserverv1.PrefixedClaimOrExpression, out *apiserver.PrefixedClaimOrExpression) error {
-	out.Claim = in.Claim
-	out.Prefix = (*string)(unsafe.Pointer(in.Prefix))
-	out.Expression = in.Expression
-	return nil
-}
+	for _, jwt := range cfg.JWT {
+		caContentProvider, err := dynamiccertificates.NewStaticCAContent("oidc-authenticator", []byte(jwt.Issuer.CertificateAuthority))
+		if err != nil {
+			return nil, fmt.Errorf("creating CA content provider: %w", err)
+		}
 
-func Convert_v1_ClaimOrExpression_To_apiserver_ClaimOrExpression(in *apiserverv1.ClaimOrExpression, out *apiserver.ClaimOrExpression) error {
-	out.Claim = in.Claim
-	out.Expression = in.Expression
-	return nil
-}
+		tokenAuthenticator, err := oidc.New(ctx, oidc.Options{
+			JWTAuthenticator:  jwt,
+			CAContentProvider: caContentProvider,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("creating token authenticator: %w", err)
+		}
 
-func Convert_v1_Issuer_To_apiserver_Issuer(in *apiserverv1.Issuer, out *apiserver.Issuer) error {
-	out.URL = in.URL
-	if err := metav1.Convert_Pointer_string_To_string(&in.DiscoveryURL, &out.DiscoveryURL, nil); err != nil {
-		return err
+		jwtAuthenticators = append(jwtAuthenticators, tokenAuthenticator)
 	}
-	out.CertificateAuthority = in.CertificateAuthority
-	out.Audiences = *(*[]string)(unsafe.Pointer(&in.Audiences))
-	out.AudienceMatchPolicy = apiserver.AudienceMatchPolicyType(in.AudienceMatchPolicy)
-	out.EgressSelectorType = apiserver.EgressSelectorType(in.EgressSelectorType)
-	return nil
+
+	return union.New(jwtAuthenticators...), nil
 }
